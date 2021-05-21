397de0772e36073c7d5e778c51ae4f1d03eeb791
riyaz <riyazu1505@gmail.com>
2021-05-21 20:47:23 +0530

    init commit

---
+++ precache-manifest.80eb40c3cf46447f9ebde20cfc225ef7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce3a6a1a4ef6737309e",
+    "url": "/riyazu1505.github.io/css/app.9ca19bd0.css"
+  },
+  {
+    "revision": "ca1321faf918e21ed953",
+    "url": "/riyazu1505.github.io/css/chunk-vendors.ca5f0e7c.css"
+  },
+  {
+    "revision": "ffe8e15214afa4231266b0bc262cca0c",
+    "url": "/riyazu1505.github.io/index.html"
+  },
+  {
+    "revision": "6ce3a6a1a4ef6737309e",
+    "url": "/riyazu1505.github.io/js/app.615e8ef9.js"
+  },
+  {
+    "revision": "1760b901345c97cdace8",
+    "url": "/riyazu1505.github.io/js/chunk-2d0e88ce.a1ffee85.js"
+  },
+  {
+    "revision": "ca1321faf918e21ed953",
+    "url": "/riyazu1505.github.io/js/chunk-vendors.7085d91b.js"
+  },
+  {
+    "revision": "e01ba63c50fe2828994f7a4ab58d29ab",
+    "url": "/riyazu1505.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/riyazu1505.github.io/robots.txt"
+  }
+]);
